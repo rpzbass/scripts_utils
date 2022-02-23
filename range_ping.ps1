e55cb9e2127322bdecff4c7a7814e598f7a1801a
rpzbass <ronaldop.tech@gmail.com>
2022-02-23 20:17:50 -0300

    basic ping range

---
+++ range_ping.ps1
@@ -0,0 +1,28 @@
+﻿<#
+  .Autor :By Ronaldo
+  .Description
+
+   Verifica a disponibilidade de IP.
+#>
+
+ipconfig /all
+
+echo " "
+echo "INFORME A REDE: ex : 192.168.0"
+echo " "
+$rede = Read-Host;
+
+echo "INFORME O FIM DO ESCOPO: "
+echo " "
+$fim = Read-Host;
+
+for ($i = 0;$i -le $fim;$i++){
+
+    ping -n 1 "$rede.$i"
+
+}
+
+
+
+
+
