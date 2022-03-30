db060b1b965f2c2b3cb325deb468957780669b20
rpzbass <ronaldop.tech@gmail.com>
2022-03-29 22:09:22 -0300

    Deleta arquivos com mais de 30 dias

---
+++ deletaMaisDeTrintaDias.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    Autor: RP
+    
+    Descrição:  Script de deleção de fotos com mais de 30 dias
+
+#>
+
+
+
+
+$path ="C:\Users\$env:USERNAME\Desktop\Powershell\Scripts\FOTOS"
+
+$fotos = Get-ChildItem | Where-Object{$_.LastWriteTime -lt (Get-Date).AddDays(-30)}
+
+foreach($foto in $fotos){
+
+   remove-item $path\$foto 
+
+}
